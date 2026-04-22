96fa3cc9d0b1ae9ff22a02089c53324b45b76003
Penine Ngizwenayo <penine.ngizwenayo@bestseller.com>
2026-04-22 18:36:35 +0200

    feat: create the threads

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class A extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("hi");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
+
 public class Main {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
 
-    static void main() {
-        System.out.println("Hello there!");
-        String str = "Big head";
-        System.out.println(str);
-
-        System.out.println( new String("Penine"));
-
-        Object numApples = 1000;
-//
-        Object value = 1000;
-        System.out.println(value.getClass().getSimpleName());
-    };
+        obj1.start();
+        obj2.start();
+    }
 }
