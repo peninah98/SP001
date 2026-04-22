6dd662dc22180f83bf95b33b8fc27c0a452f7237
Penine Ngizwenayo <penine.ngizwenayo@bestseller.com>
2026-04-22 18:45:56 +0200

    feat: scheduling the threads

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,11 @@ class A extends Thread {
     public void run() {
         for (int i = 1; i <= 10; i++) {
             System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
@@ -20,6 +25,9 @@ public class Main {
     public static void main(String[] args) {
         A obj1 = new A();
         B obj2 = new B();
+
+        obj2.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(obj1.getPriority());
 
         obj1.start();
         obj2.start();
