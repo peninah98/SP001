69029d7ec98cc2b58cc557d3798509afe506d8eb
Penine Ngizwenayo <penine.ngizwenayo@bestseller.com>
2026-05-04 18:12:51 +0200

    feat: create first lambda function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,16 @@
 package org.example;
 
-class A implements  Runnable {
+import java.util.function.Function;
+
+class A extends Thread {
     public void run() {
         for (int i = 1; i <= 10; i++) {
             System.out.println("hi");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
 
-class B implements Runnable{
+class B extends Thread{
     public void run() {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hello");
@@ -22,20 +19,15 @@ class B implements Runnable{
 }
 
 public class Main {
-    public static void main(String[] args) {
-//        A obj1 = new A();
-//        B obj2 = new B();
 
-        Runnable obj1 = new A();
-        Runnable obj2 = new B();
+    final String hello = "Hello, ";
+    Function<String, String> helloFunction = (name) ->  hello + name;
+    public  void main(String[] args) {
+    System.out.println(helloFunction.apply("Penine"));
+        A obj1 = new A();
+        B obj2 = new B();
 
-//        obj2.setPriority(Thread.MAX_PRIORITY);
-//        System.out.println(obj1.getPriority());
-
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
-
-        t1.start();
-        t2.start();
+        obj1.start();
+        obj2.start();
     }
 }
